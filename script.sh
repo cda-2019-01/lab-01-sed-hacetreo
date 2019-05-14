d1ac557a477b60a816b5e3cd33e239e2af9af252
Harol Alexander Cetre Orejuela <50224169+hacetreo@users.noreply.github.com>
2019-05-14 00:10:22 -0500

    Update script.sh
    Versión 1

--- script.sh
+++ script.sh
@@ -1,2 +1,5 @@
 # Escriba su código aquí
+awk '{print gensub(/\/([(0-9)(0-9)]*)/,"-""\\1",1)}' data.csv > out.1
+awk '{print gensub(/\/([(0-9)(0-9)]*)/,"-20""\\1",1)}' out.1 > out.2
+tr '[:lower:]'  '[:upper:]' < out.2  | awk '{print gensub(/([0-9][0-9])-([0-9][0-9])-([0-9][0-9][0-9][0-9])/, " \\3-\\2-\\1", 1)}' | sed 's/;;/;\N;/g' | sed 's/,/./g'| sed 's/;/,/g' | sed 's/,N/,\\N/g' | sed 's/,/./4g' | sed "/\N/ d"  
 rm out.*
